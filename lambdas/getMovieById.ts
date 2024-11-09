c997320062ce07409c7779c388dda0761e8d0250
WhatIsRove <whatisbuff@gmail.com>
2024-11-09 17:47:14 +0000

    Exercise: Expand get movie endpoint

--- lambdas/getMovieById.ts
+++ lambdas/getMovieById.ts
@@ -1,7 +1,7 @@
 import { Handler } from "aws-lambda";
 
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, QueryCommand, QueryCommandInput } from "@aws-sdk/lib-dynamodb";
 
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
@@ -13,6 +13,7 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
     console.log("Event: ", JSON.stringify(event));
     const parameters = event?.pathParameters;
     const movieId = parameters?.movieId ? parseInt(parameters.movieId) : undefined;
+    const cast = event.queryStringParameters?.cast === "true";
 
     if (!movieId) {
       return {
@@ -40,9 +41,37 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
         body: JSON.stringify({ Message: "Invalid movie Id" }),
       };
     }
-    const body = {
-      data: commandOutput.Item,
-    };
+
+    let body = {};
+    let castInput: QueryCommandInput = {
+      TableName: process.env.CAST_TABLE_NAME,
+    }
+    if (cast) {
+      castInput = {
+        ...castInput,
+        KeyConditionExpression: "movieId = :m",
+        ExpressionAttributeValues: {
+          ":m": movieId,
+        },
+      }
+      const castOutput = await ddbDocClient.send(
+        new QueryCommand(castInput)
+      );
+
+      body = {
+        data: {
+          ...commandOutput.Item,
+          ...castOutput.Items
+        }
+      };
+    } else {
+      body = {
+        data: commandOutput.Item,
+      };
+    }
+    
+
+    
 
     // Return Response
     return {
